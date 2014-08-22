fcda102cfc6c4e8d6023db89994fb579287b5b5a
Dandi_Chen <progmdong@163.com>
2014-08-22 15:01:29 +0800

    Spectral Clustering 算法

---
+++ PCM/spectral_clustering.h
@@ -0,0 +1,21 @@
+#ifndef _SPECTRAL_CLUSTERING_H
+#define _SPECTRAL_CLUSTERING_H
+
+#include <QThread>
+#include "sample_set.h"
+
+class SpectralClusteringThread : public QThread
+{
+	Q_OBJECT
+
+public:
+	void run() Q_DECL_OVERRIDE;
+
+signals:
+	void finish_compute();
+
+
+};
+
+
+#endif
